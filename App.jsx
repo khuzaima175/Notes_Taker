79321b84f858ce939ffc6fa7f57504e9bcd074a1
Khuzaima <75350696+khuzaima175@users.noreply.github.com>
2024-08-09 17:02:31 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "./Header";
+import Footer from "./footer";
+import Note from "./note";
+
+function App(){
+    return <div>
+        <Header/>
+        <Footer/>
+        <Note/>
+    </div>
+}
+
+export default App;
